0667aefc80f96606679e0bd2aa8d5fe33e9ff217
Chris Done <chrisdone@gmail.com>
2018-01-29 14:40:15 +0000

    Safe API

--- src/ODBC.hs
+++ src/ODBC.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE ViewPatterns #-}
 {-# LANGUAGE MultiWayIf #-}
@@ -9,18 +11,18 @@
 -- | Trivial bindings to ODBC for testing purposes.
 
 module ODBC
-  ( withEnv
-  , withDbc
-  , queryRows
-  , queryDiscarding
-  , preparedExec
+  (
+    -- * Connection
+    connect
+    -- * Executing queries
+  , exec
+  , query
+    -- * Types
   , Value(..)
-  , SQLHSTMT
-  , SQLHDBC
-  , SQLHENV
   ) where
 
 import           Control.Concurrent.Async
+import           Control.Concurrent.MVar
 import           Control.DeepSeq
 import           Control.Exception
 import qualified Data.ByteString as S
@@ -49,11 +51,13 @@ sql_no_data = RETCODE 100
 --------------------------------------------------------------------------------
 -- Types
 
+-- | A database exception.
 data ODBCException
-  = UnsuccessfulReturnCode String RETCODE
-  | AllocationReturnedNull String
-  | UnknownType Int16
-  deriving (Typeable, Show)
+  = UnsuccessfulReturnCode !String !RETCODE
+  | AllocationReturnedNull !String
+  | UnknownType !Int16
+  | DatabaseIsClosed !String
+  deriving (Typeable, Show, Eq)
 instance Exception ODBCException
 
 -- | A column description.
@@ -73,42 +77,114 @@ data Value
   deriving (Eq, Show, Typeable, Ord, Generic, Data)
 instance NFData Value
 
+-- | Connection to a database.
+newtype Connection = Connection
+  {connectionMVar :: MVar (Maybe ConnectionState)}
+
+-- | Container of unsafe pointers. This value should only be accessed
+-- from within the 'Connection' type.
+data ConnectionState = ConnectionState
+  { connectionEnv :: !(ForeignPtr SQLHENV)
+  , connectionDbc  :: !(ForeignPtr SQLHDBC)
+  }
+
 --------------------------------------------------------------------------------
 -- Exposed functions
 
-preparedExec :: SQLHDBC t -> String -> IO [[Value]]
-preparedExec dbc string =
-  withPrepare
-    dbc
-    string
-    (\stmt -> do
-       assertSuccess "odbc_SQLExec" (odbc_SQLExec stmt)
-       fetchStatementRows stmt)
+-- | Connect using the given connection string.
+connect ::
+     String -- ^ Connection string.
+  -> IO Connection
+connect string =
+  withBound
+    (do env <-
+          uninterruptibleMask_
+            (do ptr <- assertNotNull "odbc_SQLAllocEnv" odbc_SQLAllocEnv
+                newForeignPtr odbc_SQLFreeEnv (coerce ptr))
+        assertSuccess "odbc_SetEnvAttr" (withForeignPtr env odbc_SetEnvAttr)
+        -- Above: Allocate the environment.
+        -- Below: Allocate a database handle, not connected.
+        dbc <-
+          uninterruptibleMask_
+            (do ptr <-
+                  assertNotNull
+                    "odbc_SQLAllocDbc"
+                    (withForeignPtr env odbc_SQLAllocDbc)
+                newForeignPtr odbc_SQLFreeDbc (coerce ptr))
+        -- Below: Try to connect to the database.
+        withCString
+          string
+          (\cstring ->
+             uninterruptibleMask_
+               (do assertSuccess
+                     "odbc_SQLDriverConnect"
+                     (withForeignPtr
+                        dbc
+                        (\dbcPtr -> odbc_SQLDriverConnect dbcPtr cstring))
+                   addForeignPtrFinalizer odbc_SQLDisconnect dbc))
+        -- Below: Keep the environment and the database handle in an mvar.
+        mvar <-
+          newMVar
+            (Just (ConnectionState {connectionEnv = env, connectionDbc = dbc}))
+        pure (Connection mvar))
 
--- | Query, process and discard all rows.
-queryDiscarding :: SQLHDBC t -> String -> IO Int
-queryDiscarding dbc string =
-  withExecDirect
-    dbc
-    string
-    (\stmt -> do
-       let loop !rows = do
-             do retcode0 <- odbc_SQLFetch stmt
-                if | retcode0 == sql_no_data ->
-                     do retcode <- odbc_SQLMoreResults stmt
-                        if retcode == sql_success ||
-                           retcode == sql_success_with_info
-                          then loop rows
-                          else pure rows
-                   | retcode0 == sql_success ||
-                       retcode0 == sql_success_with_info -> loop (rows + 1)
-                   | otherwise ->
-                     throwIO (UnsuccessfulReturnCode "odbc_SQLFetch" retcode0)
-       loop 0)
+-- | Exec a statement on the database.
+exec :: Connection -> String -> IO ()
+exec conn string =
+  withBound
+    (withHDBC conn "exec" (\dbc -> withExecDirect dbc string (const (pure ()))))
 
--- | Query and return all rows.
-queryRows :: SQLHDBC t -> String -> IO [[Value]]
-queryRows dbc string = withExecDirect dbc string fetchStatementRows
+-- | Query and produce a strict list.
+query :: Connection -> String -> IO [[Value]]
+query conn string =
+  withBound
+    (withHDBC
+       conn
+       "query"
+       (\dbc -> withExecDirect dbc string fetchStatementRows))
+
+--------------------------------------------------------------------------------
+-- Internal wrapper functions
+
+-- | Thread-safely access the connection pointer.
+withHDBC :: Connection -> String -> (Ptr SQLHDBC -> IO a) -> IO a
+withHDBC conn label f =
+  withMVar
+    (connectionMVar conn)
+    (\mfptr ->
+       case mfptr of
+         Nothing -> throwIO (DatabaseIsClosed label)
+         Just (ConnectionState {connectionDbc = db,connectionEnv=env}) -> do
+           v <- withForeignPtr db f
+           touchForeignPtr db
+           touchForeignPtr env
+           pure v)
+
+-- | Execute a query directly without preparation.
+withExecDirect :: Ptr SQLHDBC -> String -> (forall s. SQLHSTMT s -> IO a) -> IO a
+withExecDirect dbc string cont =
+  withStmt
+    dbc
+    (\stmt -> do
+       assertSuccess
+         "odbc_SQLExecDirect"
+         (withCString string (odbc_SQLExecDirect stmt))
+       cont stmt)
+
+-- | Run the function with a statement.
+withStmt :: Ptr SQLHDBC -> (forall s. SQLHSTMT s -> IO a) -> IO a
+withStmt hdbc =
+  bracket
+    (assertNotNull "odbc_SQLAllocStmt" (odbc_SQLAllocStmt hdbc))
+    odbc_SQLFreeStmt
+
+-- | Run an action in a bound thread. This is neccessary due to the
+-- interaction with signals in ODBC and GHC's runtime.
+withBound :: IO a -> IO a
+withBound = flip withAsyncBound wait
+
+--------------------------------------------------------------------------------
+-- Internal data retrieval functions
 
 -- | Fetch all rows from a statement.
 fetchStatementRows :: SQLHSTMT s -> IO [[Value]]
@@ -137,7 +213,8 @@ fetchStatementRows stmt = do
                 throwIO (UnsuccessfulReturnCode "odbc_SQLFetch" retcode0)
   loop id
 
-describeColumn :: Integral a => SQLHSTMT s -> a -> IO Column
+-- | Describe the given column by its integer index.
+describeColumn :: SQLHSTMT s -> Int16 -> IO Column
 describeColumn stmt i =
   withCStringLen
     (replicate 100 '\0')
@@ -176,7 +253,7 @@ describeColumn stmt i =
                                   , columnNull = isnull
                                   }))))))
 
-
+-- | Pull data for the given column.
 getData :: SQLHSTMT s -> Int -> Column -> IO Value
 getData stmt i col =
   case columnType col of
@@ -247,63 +324,6 @@ getData stmt i col =
            bufferlen
            strlenp)
 
-withPrepare :: SQLHDBC t -> String -> (forall s. SQLHSTMT s -> IO a) -> IO a
-withPrepare dbc string cont =
-  withStmt
-    dbc
-    (\stmt -> do
-       assertSuccess
-         "odbc_SQLPrepare"
-         (withCString string (odbc_SQLPrepare stmt))
-       cont stmt)
-
-withExecDirect :: SQLHDBC t -> String -> (forall s. SQLHSTMT s -> IO a) -> IO a
-withExecDirect dbc string cont =
-  withStmt
-    dbc
-    (\stmt -> do
-       assertSuccess
-         "odbc_SQLExecDirect"
-         (withCString string (odbc_SQLExecDirect stmt))
-       cont stmt)
-
--- | Run the function with an environment.
-withEnv :: (forall s. SQLHENV s -> IO a) -> IO a
-withEnv cont =
-  bracket
-    (assertNotNull "odbc_SQLAllocEnv" odbc_SQLAllocEnv)
-    odbc_SQLFreeEnv
-    (\env -> do
-       assertSuccess "odbc_SetEnvAttr" (odbc_SetEnvAttr env)
-       cont env)
-
--- | Run the function with a database handle.
-withDbc :: SQLHENV t -> String -> (forall s. SQLHDBC s -> IO a) -> IO a
-withDbc henv connString cont =
-  bracket
-    (assertNotNull "odbc_SQLAllocDbc" (odbc_SQLAllocDbc henv))
-    odbc_SQLFreeDbc
-    (\dbc ->
-       withBound
-         (do bracket
-               (withCString
-                  connString
-                  (assertSuccess "odbc_SQLDriverConnect" .
-                   odbc_SQLDriverConnect dbc))
-               (const (odbc_SQLDisconnect dbc))
-               (const (cont dbc))))
-
--- | Run the function with a statement.
-withStmt :: SQLHDBC t -> (forall s. SQLHSTMT s -> IO a) -> IO a
-withStmt hdbc =
-  bracket
-    (assertNotNull "odbc_SQLAllocStmt" (odbc_SQLAllocStmt hdbc))
-    odbc_SQLFreeStmt
-
--- | Run an action in a bound thread.
-withBound :: IO a -> IO a
-withBound = flip withAsyncBound wait
-
 --------------------------------------------------------------------------------
 -- Correctness checks
 
@@ -330,9 +350,16 @@ assertSuccess label m = do
 
 -- Opaque pointers
 
-newtype SQLHENV s = SQLHENV (Ptr (SQLHENV s))
-newtype SQLHDBC s = SQLHDBC (Ptr (SQLHDBC s))
+-- | An environment. Allocated once per connection.
+data SQLHENV
+
+-- | A database connection.
+data SQLHDBC
+
+-- | The handle allocated for any query.
 newtype SQLHSTMT s = SQLHSTMT (Ptr (SQLHSTMT s))
+
+-- | Used to get data.
 newtype SQLPOINTER = SQLPOINTER (Ptr SQLPOINTER)
 
 -- https://github.com/Microsoft/ODBC-Specification/blob/753d7e714b7eab9eaab4ad6105fdf4267d6ad6f6/Windows/inc/sqltypes.h#L152
@@ -361,40 +388,34 @@ newtype SQLULEN = SQLULEN Word64 deriving (Show, Eq, Storable)
 -- Foreign functions
 
 foreign import ccall "odbc odbc_SQLAllocEnv"
-  odbc_SQLAllocEnv :: IO (SQLHENV s)
+  odbc_SQLAllocEnv :: IO (Ptr SQLHENV)
 
-foreign import ccall "odbc odbc_SQLFreeEnv"
-  odbc_SQLFreeEnv :: SQLHENV s -> IO ()
+foreign import ccall "odbc &odbc_SQLFreeEnv"
+  odbc_SQLFreeEnv :: FunPtr (Ptr SQLHENV -> IO ())
 
 foreign import ccall "odbc odbc_SetEnvAttr"
-  odbc_SetEnvAttr :: SQLHENV s -> IO RETCODE
+  odbc_SetEnvAttr :: Ptr SQLHENV -> IO RETCODE
 
 foreign import ccall "odbc odbc_SQLAllocDbc"
-  odbc_SQLAllocDbc :: SQLHENV s -> IO (SQLHDBC s)
+  odbc_SQLAllocDbc :: Ptr SQLHENV -> IO (Ptr SQLHDBC)
 
-foreign import ccall "odbc odbc_SQLFreeDbc"
-  odbc_SQLFreeDbc :: SQLHDBC s -> IO ()
+foreign import ccall "odbc &odbc_SQLFreeDbc"
+  odbc_SQLFreeDbc :: FunPtr (Ptr SQLHDBC -> IO ())
 
 foreign import ccall "odbc odbc_SQLDriverConnect"
-  odbc_SQLDriverConnect :: SQLHDBC s -> CString -> IO RETCODE
+  odbc_SQLDriverConnect :: Ptr SQLHDBC -> CString -> IO RETCODE
 
-foreign import ccall "odbc odbc_SQLDisconnect"
-  odbc_SQLDisconnect :: SQLHDBC s -> IO ()
+foreign import ccall "odbc &odbc_SQLDisconnect"
+  odbc_SQLDisconnect :: FunPtr (Ptr SQLHDBC -> IO ())
 
 foreign import ccall "odbc odbc_SQLAllocStmt"
-  odbc_SQLAllocStmt :: SQLHDBC s -> IO (SQLHSTMT s)
+  odbc_SQLAllocStmt :: Ptr SQLHDBC -> IO (SQLHSTMT s)
 
 foreign import ccall "odbc odbc_SQLFreeStmt"
   odbc_SQLFreeStmt :: SQLHSTMT s -> IO ()
 
 foreign import ccall "odbc odbc_SQLExecDirect"
   odbc_SQLExecDirect :: SQLHSTMT s -> CString -> IO RETCODE
-
-foreign import ccall "odbc odbc_SQLExec"
-  odbc_SQLExec :: SQLHSTMT s -> IO RETCODE
-
-foreign import ccall "odbc odbc_SQLPrepare"
-  odbc_SQLPrepare :: SQLHSTMT s -> CString -> IO RETCODE
 
 foreign import ccall "odbc odbc_SQLFetch"
   odbc_SQLFetch :: SQLHSTMT s -> IO RETCODE
